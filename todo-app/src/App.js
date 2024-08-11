a42aca59c1f5fee92bb9967bcbab04136131c2db
sina-matin-rad <122496889+sina-matin-rad@users.noreply.github.com>
2024-08-11 16:39:55 +0330

    Provider setting in the main program

--- todo-app/src/App.js
+++ todo-app/src/App.js
@@ -1,24 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+// src/App.js
+import React from 'react';
+import { Provider } from 'react-redux';
+import { store } from './redux/store';
+import TaskList from './components/TaskList';
+import AddTaskForm from './components/AddTaskForm';
+import { Container, Typography } from '@mui/material';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Provider store={store}>
+      <Container>
+        <Typography variant="h2" gutterBottom>Task Manager</Typography>
+        <AddTaskForm />
+        <TaskList />
+      </Container>
+    </Provider>
   );
 }
 
